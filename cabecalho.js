7cf2cf7b14c08cd2e6b02d7f6a835948400c4935
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2021-05-23 18:49:26 -0300

    Add files via upload

--- cabecalho.js
+++ cabecalho.js
@@ -3,14 +3,14 @@ function cabi(){
     var cabecalho = window.document.getElementById('cabecalho')
     var htmlcab = `
     <nav class="main" id="cabecalhoinfo">
-        <a href="http://comdema.wederson.net.br/">
+        <a href="http://palpiteiros.wederson.net.br/">
             <nav class="titulo"><span id="hie1">Palpiteiros</span></nav>
         </a>
         <nav id="menu">
             <ul class="menu">
-                <a href="http://comdema.wederson.net.br/"><li>Resultados</li></a>
-                <a href="http://comdema.wederson.net.br/stand/"><li>Classificação</li></a></a>
-                <a href="http://comdema.wederson.net.br/aposta/"><li>Aposta</li></a></a>
+                <a href="http://palpiteiros.wederson.net.br/"><li>Resultados</li></a>
+                <a href="http://palpiteiros.wederson.net.br/stand/"><li>Classificação</li></a></a>
+                <a href="http://palpiteiros.wederson.net.br/aposta/"><li>Aposta</li></a></a>
             </ul>
         </nav>
     </nav>
