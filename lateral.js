deb89b24064fdb50428f543309a7cf03414be209
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2021-05-23 18:39:27 -0300

    Add files via upload

---
+++ lateral.js
@@ -0,0 +1,20 @@
+function laterali(){
+    var lateral = window.document.getElementById('secundaria')
+    var htmllateral = `
+    <nav id="ligacoes"><input type="button" classe="apagar" value="X" onclick="apagar()"><br>
+            <ul class="lateral"><h2>Links Institucionais</h2>
+                <a href="https://www.cmbj.mg.gov.br/"><li>Poder Legislativo</li></a>
+                <a href="https://www.bomjardimdeminas.mg.gov.br/"><li>Poder Executivo</li></a>
+            </ul>
+            <ul class="lateral"><h2>Redes Sociais</h2>
+                <a href="https://www.facebook.com/agriculturapecuariaemeioambientebomjardimdeminas"><li>Facebook SM</li></a>
+            </ul>
+        </nav>
+    `;
+   lateral.innerHTML = htmllateral
+}
+laterali()
+function apagar(){
+    var ligacoes = window.document.getElementById('ligacoes')
+    ligacoes.style.display = "none"
+}
