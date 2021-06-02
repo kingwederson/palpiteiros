61d75f490289b9e4f24be59a4f5624ee3170f8e2
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2021-06-02 12:56:59 -0300

    Add files via upload

--- rodape.js
+++ rodape.js
@@ -2,7 +2,6 @@ function rodapi(){
     var rodape = window.document.getElementById('rodape')
     var htmlrodape = `
     <nav class="main" id="rodapeinfo">
-        <p class="lista"><b>Disclamer</b>: As informaçõe contidas neste site não são recomentação de investimento.</p>
         <p class="lista">wederson.net.br, 2021.</p>
     </nav>
     `;
